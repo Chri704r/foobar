85f5ac7f87e70bb3b19b75ec57c4e4503694d529
sand4570 <sand4570@stud.kea.dk>
2021-11-26 10:27:09 +0100

    Data fetch loop

--- dashbord/main.js
+++ dashbord/main.js
@@ -2,9 +2,25 @@ import "./style.scss";
 
 window.addEventListener("DOMContentLoaded", init);
 
-async function init() {
-  const response = await fetch("https://groupfoobar.herokuapp.com/");
+function init() {
+  getData();
+}
+
+async function getData() {
+  const dataUrl = "https://groupfoobar.herokuapp.com/";
+  const beerUrl = "https://groupfoobar.herokuapp.com/beertypes";
+
+  const data = await fetchfunction(dataUrl);
+  const beerData = await fetchfunction(beerUrl);
+  console.log("data", data);
+  console.log("beer", beerData);
+
+  setTimeout(getData, 5000);
+}
+
+async function fetchfunction(url) {
+  const response = await fetch(url);
 
   const data = await response.json();
-  console.log("data", data);
+  return data;
 }
