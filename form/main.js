d97b43fe8f8fa99bd412a4bc2f836cfe999413c4
Katrín María <katr240j@stud.kea.dk>
2021-11-26 12:38:34 +0100

    first try

--- form/main.js
+++ form/main.js
@@ -1,1 +1,26 @@
 import "./style.scss";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  getData();
+}
+
+async function getData() {
+  const dataUrl = "https://groupfoobar.herokuapp.com/";
+  const beerUrl = "https://groupfoobar.herokuapp.com/beertypes";
+
+  const data = await fetchfunction(dataUrl);
+  const beerData = await fetchfunction(beerUrl);
+  console.log("data", data);
+  console.log("beer", beerData);
+
+  setTimeout(getData, 5000);
+}
+
+async function fetchfunction(url) {
+  const response = await fetch(url);
+
+  const data = await response.json();
+  return data;
+}
