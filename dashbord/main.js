5c37316139082bad74b45714bd7d816745561ca2
Chri704r <christinepiilmann8@gmail.com>
2021-11-28 00:02:27 +0100

    clock js added

--- dashbord/main.js
+++ dashbord/main.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
 	getData();
+	setInterval(setDate, 1000);
 }
 
 async function getData() {
@@ -34,8 +35,15 @@ function showQueue(data) {
 	data.queue.forEach((order) => {
 		const clone = document.querySelector("template.customer-dashboard").content.cloneNode(true);
 
-		clone.querySelector(".order").textContent = order.order;
 		clone.querySelector(".ordernumber").textContent = order.id;
+
+		//show each beer in order
+		order.order.forEach((orderlist) => {
+			const orderContainer = document.createElement("p");
+			orderContainer.textContent = orderlist;
+
+			clone.querySelector(".order").appendChild(orderContainer);
+		});
 
 		document.querySelector("#queue-container").appendChild(clone);
 	});
@@ -47,9 +55,45 @@ function showServing(data) {
 	data.serving.forEach((serve) => {
 		const clone = document.querySelector("template.customer-dashboard").content.cloneNode(true);
 
-		clone.querySelector(".order").textContent = serve.order;
 		clone.querySelector(".ordernumber").textContent = serve.id;
+
+		//show each beer in order
+		serve.order.forEach((orderlist) => {
+			const orderContainer = document.createElement("p");
+			orderContainer.textContent = orderlist;
+
+			clone.querySelector(".order").appendChild(orderContainer);
+		});
 
 		document.querySelector("#serving-container").appendChild(clone);
 	});
+}
+
+function setDate() {
+	//select hour, minutes and seconds and create variables
+	const secondHand = document.querySelector(".clock-seconds");
+	const minuteHand = document.querySelector(".clock-minute");
+	const hourHand = document.querySelector(".clock-hour");
+
+	//get current date and time
+	const now = new Date();
+
+	//get current seconds, minutes and hour
+	const seconds = now.getSeconds();
+	const minutes = now.getMinutes();
+	const hours = now.getHours();
+
+	//calculate degrees for seconds, minutes and hours
+	const secondsDegrees = (seconds / 60) * 360 + 90;
+	//make the hands progressively move with seconds/minutes
+	const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+	const hourDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+
+	//add style to the hands
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+	//show digital clock
+	document.querySelector(".digital-clock").innerHTML = `${hours}:${minutes}:${seconds}`;
 }
