cc08137e181c963b26a1a6b8a4f7c6fef9fb0551
sand4570 <sand4570@stud.kea.dk>
2021-12-12 12:17:18 +0100

    calender bug fox

--- dashbord/barchart.js
+++ dashbord/barchart.js
@@ -613,10 +613,12 @@ async function displayEventMarkers() {
   const eData = await fetchfunction("events.json");
 
   eData.forEach((event) => {
-    if (calendarData.currentMonth == event.date.month) {
-      document
-        .querySelector(`td[id="${event.date.day}"]`)
-        .classList.add("has-event");
+    if (calendarData.currentYear == event.date.year) {
+      if (calendarData.currentMonth == event.date.month) {
+        document
+          .querySelector(`td[id="${event.date.day}"]`)
+          .classList.add("has-event");
+      }
     }
   });
 }
@@ -627,26 +629,28 @@ function displayEvents(events, selector) {
   let hasEvent = false;
 
   events.forEach((event) => {
-    if (calendarData.currentMonth == event.date.month) {
-      console.log("Is in december");
-      if (calendarData.currentDay == event.date.day) {
-        console.log("its the right day");
+    if (calendarData.currentYear == event.date.year) {
+      if (calendarData.currentMonth == event.date.month) {
+        console.log("Is in december");
+        if (calendarData.currentDay == event.date.day) {
+          console.log("its the right day");
 
-        const clone = document
-          .querySelector("#temp-event")
-          .content.cloneNode(true);
+          const clone = document
+            .querySelector("#temp-event")
+            .content.cloneNode(true);
 
-        //Insert events in clone
-        clone.querySelector(".title").textContent = event.name;
-        clone.querySelector(".description").textContent = event.description;
-        clone.querySelector(
-          ".time"
-        ).textContent = `${event.time.start} - ${event.time.end}`;
+          //Insert events in clone
+          clone.querySelector(".title").textContent = event.name;
+          clone.querySelector(".description").textContent = event.description;
+          clone.querySelector(
+            ".time"
+          ).textContent = `${event.time.start} - ${event.time.end}`;
 
-        //Append clone to section
-        selector.appendChild(clone);
+          //Append clone to section
+          selector.appendChild(clone);
 
-        hasEvent = true;
+          hasEvent = true;
+        }
       }
     }
   });
