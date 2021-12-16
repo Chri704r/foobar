b1dedbc43abc5174731fa7bda8977b14e3be25f7
sand4570 <sand4570@stud.kea.dk>
2021-12-16 18:05:43 +0100

    building

--- dashbord/vite.config.js
+++ dashbord/vite.config.js
@@ -1,15 +1,15 @@
 // vite.config.js
-const { resolve } = require('path')
-const { defineConfig } = require('vite')
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
-    base: '/kea/14_eksamen/dashb/',
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                barchart: resolve(__dirname, 'dashboard.html')
-            }
-        },
-    }
-})
+  base: "/kea/14_eksamen/dash_b/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        barchart: resolve(__dirname, "dashboard.html"),
+      },
+    },
+  },
+});
